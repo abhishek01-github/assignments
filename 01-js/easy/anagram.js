183e16da4e0bece8497327d9efdd0ad5d095ff91
Abhishek Pandey <abhishekpandey118852@gmail.com>
2024-06-13 16:50:03 +0530

    committed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,30 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toUpperCase();
+  str2 = str2.toUpperCase();
+  
+  if(str1.length!=str2.length){
+    return false;
+  }
+  
+  let map = [];
+  
+  for(let i=0;i<256;i++){
+    map[i] = 0;
+  }
+ 
+  for(let i=0;i<str1.length;i++){
+    map[str1.charCodeAt(i)]++;
+    map[str2.charCodeAt(i)]--;
+  }
 
+  for(let i=0;i<map.length;i++){
+    if(map[i]!=0){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
